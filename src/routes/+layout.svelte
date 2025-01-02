e878f257225c8d75868c53bc846b2d599eb2068e
spences10 <spences10apps@gmail.com>
2025-01-02 18:08:20 +0000

    chore: :arrow_up: update deps

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
-<script>
+<script lang="ts">
 	import '../app.postcss'
+	interface Props {
+		children?: import('svelte').Snippet;
+	}
+
+	let { children }: Props = $props();
 </script>
 
 <main class="container max-w-3xl mx-auto px-4 prose prose-xl">
-	<slot />
+	{@render children?.()}
 </main>
